e780600d8d5d7f3a871745059fd5720a2898ff5a
Leonildo-Gomes <leogomescv@gmail.com>
2025-06-03 13:06:28 +0200

    feat: add swipper in detailCar

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'swiper/css'; // Importa o estilo base
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import { register } from 'swiper/element/bundle'
 import App from './App.tsx'
 import AuthProvider from './context/AuthContext'
 import './index.css'
+
+register (); 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider> 
--- src/pages/car/index.tsx
+++ src/pages/car/index.tsx
@@ -1,7 +1,10 @@
 import { doc, getDoc } from 'firebase/firestore';
 import { useEffect, useState } from "react";
 import { FaWhatsapp } from 'react-icons/fa';
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Container } from "../../components/container";
 import { db } from "../../services/firebaseConnection";
   interface CarProps {
@@ -27,6 +30,8 @@ interface CarImageProps {
 export function CarDetail() { 
     const [car, setCar] = useState<CarProps>(); 
     const {id} = useParams(); 
+    const [sliderPerview , setSliderPerview ] = useState<number>(2); 
+    const navigate=useNavigate();
 
     useEffect(()=> {
         async function getCar() {
@@ -35,6 +40,10 @@ export function CarDetail() {
             getDoc(docRef)
             .then((snapshot) => {
                 console.log(snapshot.data())
+                if(!snapshot.exists()) {
+                    navigate("/"); 
+                    return
+                } 
                 setCar({ 
                     id: snapshot.id,
                     name: snapshot.data()?.name,
@@ -55,11 +64,40 @@ export function CarDetail() {
         } 
         getCar(); 
          
-    }, [id]); 
+    }, []); 
+    useEffect (() => {
+        function handleRezise() {
+             if(window.innerWidth < 720) {
+                setSliderPerview(1); 
+             } else {
+                setSliderPerview(2); 
+             }
+        } 
+        
+        handleRezise(); 
+        window.addEventListener('resize', handleRezise); 
+        return () => {
+            window.removeEventListener('resize', handleRezise); 
+        } 
+        
+    }, []) 
     return ( 
        <Container >
-            <h1>TTT</h1>
-
+            { car && (
+                 <Swiper slidesPerView={sliderPerview} 
+                    pagination={{clickable: true}} 
+                    navigation
+                    modules={[Navigation, Pagination]}
+                >
+                    {car?.images.map(image =>(
+                        <SwiperSlide key={image.name}> 
+                            <img className='w-full h-96 object-cover' 
+                            src={image.url} alt="" 
+                            /> 
+                        </SwiperSlide> 
+                    ))} 
+                </Swiper>   
+            ) }
             { car && (
                 <main className='w-full bg-white rounded-lg p-6 my-4'>
                     <div className='flex flex-col sm:flex-row mb-4 items-center justify-between '>
@@ -95,7 +133,10 @@ export function CarDetail() {
                     <p>{car?.whatsapp} </p>
 
 
-                    <a  className=' w-full bg-green-500 text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-lg font-medium  cursor-pointer'>
+                    <a  className=' w-full bg-green-500 text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-lg font-medium  cursor-pointer'
+                        href={`https://api.whatsapp.com/send?phone=${car?.whatsapp}&text= Ola vie esse carro ${car?.name} no site WebCarros e fiquei interessado!`}  
+                        target='_blank' 
+                    >
                         Conversar com o Vendedor
                         <FaWhatsapp size={24} color='#FFF'/> 
                     </a>
